00d46f1fdd087d905de42a71c9f0ae2062832c40
nipunaluthge <nipunaluthge4815@gmail.com>
2020-11-03 02:31:31 +0530

    user schema added

--- models/User.js
+++ models/User.js
@@ -1,5 +1,27 @@
 const mongoose = require('mongoose');
 
+const skillsSchema = new mongoose.Schema({
+  category: String,
+  description: String
+});
+
+const ratingSchema = new mongoose.Schema({
+  rating: Number,
+  jobId: mongoose.Types.ObjectId, //add a ref //////////////////////////////////////////////// 
+});
+
+const savedJobsSchema = new mongoose.Schema({
+  jobPostId: mongoose.Types.ObjectId, // add a ref ///////////////////////////////////////////
+});
+
+const appliedJobs = new mongoose.Schema({ 
+  jobPostId: mongoose.Types.ObjectId, // add a ref ///////////////////////////////////////////
+  status: { //pending accepted rejected
+    type: String, 
+    default: 'pending'
+  }
+})
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -20,6 +42,35 @@ const userSchema = new mongoose.Schema({
   activeStatus: {
     type: Date
   },
-
+  bio: {
+    type: String,
+  },
+  level: {
+    type: Number,
+    default: 1
+  },
+  verifyStatus: {
+    type: Boolean,
+    default: false
+  },
+  jobsCount: {
+    type: Number,
+    default: 0
+  },
+  skills: {
+    type: [skillsSchema],
+  },
+  providerRating: {
+    type: [ratingSchema],
+  },
+  seekerRating: {
+    type: [ratingSchema],
+  },
+  savedJobPosts: {
+    type: [savedJobsSchema],
+  },
+  appliedJobs: {
+    type: [appliedJobs],
+  }
 
 }, {timestamps: true})

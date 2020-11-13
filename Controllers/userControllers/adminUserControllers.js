8d983676f30431140a07ded50536aef4ca54c43c
nipunaluthge <nipunaluthge4815@gmail.com>
2020-11-14 00:39:45 +0530

    admin protection added

--- Controllers/userControllers/adminUserControllers.js
+++ Controllers/userControllers/adminUserControllers.js
@@ -1,5 +1,5 @@
 const User = require('../../models/User');
-const { checkAccess } = require('../../services/Authentication');
+const { checkAccess, getJwtInfo } = require('../../services/Authentication');
 
 /////////////////////////////////////// FIND USER TO DASHBOARD TO EDIT ///////////////////////////////////
 
@@ -12,12 +12,16 @@ const findAUser = async (req, res) => {
       foundUser = await User.findOne({ contactNo: req.query.contactNo });
     }
 
-    if (foundUser.email) {
-      const { _id, name, email, contactNo, role, isDissable, verifyStatus } = foundUser;
-      res.status(200).json({ _id, name, email, contactNo, role, isDissable, verifyStatus });
+    if (foundUser) {
+      if (foundUser.email) {
+        const { _id, name, email, contactNo, role, isDissable, verifyStatus } = foundUser;
+        res.status(200).json({ _id, name, email, contactNo, role, isDissable, verifyStatus });
+      } else {
+        const { _id, name, contactNo, role, isDissable, verifyStatus } = foundUser;
+        res.status(200).json({ _id, name, email: '', contactNo, role, isDissable, verifyStatus });
+      }
     } else {
-      const { _id, name, contactNo, role, isDissable, verifyStatus } = foundUser;
-      res.status(200).json({ _id, name, email: '', contactNo, role, isDissable, verifyStatus });
+      res.status(404).json({ error: 'No user Found!' });
     }
     
   } catch (err) {
@@ -29,39 +33,46 @@ const findAUser = async (req, res) => {
 ////////////////////////////////////// EDIT USER IN THE DASHBOARD ///////////////////////////////////////////////
 
 const editUser = async (req, res) => {
-  try {
+  try { 
     let editedUser;
     const { _id ,role, verifyStatus, isDissable } = req.body;
-    if (checkAccess('P0105') && checkAccess('P0201')) {
-      console.log('here');
-      editedUser = await User.updateOne(
-        { _id: _id },
-        { $set: { 
-          role: role,
-          verifyStatus: verifyStatus,
-          isDissable: isDissable
-        } }
-      )
-    } else if (checkAccess('P0105')) {
-      editedUser = await User.updateOne(
-        { _id: _id },
-        { $set: { 
-          role: role,
-        } }
-      )
-    } else if (checkAccess('P0201')) {
-      editedUser = await User.updateOne(
-        { _id: _id },
-        { $set: { 
-          verifyStatus: verifyStatus,
-          isDissable: isDissable
-        } }
-      )
+    let user = await User.findOne({ _id: _id });
+    if ( user.role !== 'Admin' || (user.role === 'Admin' && getJwtInfo(req).role.title === 'Admin')) {
+      
+      if (checkAccess('P0105') && checkAccess('P0201')) {
+          editedUser = await User.updateOne(
+            { _id: _id },
+            { $set: { 
+              role: role,
+              verifyStatus: verifyStatus,
+              isDissable: isDissable
+            } }
+          )
+        } else if (checkAccess('P0105')) {
+          editedUser = await User.updateOne(
+            { _id: _id },
+            { $set: { 
+              role: role,
+            } }
+          )
+        } else if (checkAccess('P0201')) {
+          editedUser = await User.updateOne(
+            { _id: _id },
+            { $set: { 
+              verifyStatus: verifyStatus,
+              isDissable: isDissable
+            } }
+          )
+        } else {
+          res.status(401).json({ error: 'Unauthorized access' });
+        }
+        res.status(204).json( editedUser );
     } else {
       res.status(401).json({ error: 'Unauthorized access' });
     }
+    
 
-    res.status(204).json( editedUser );
+    
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: 'Somthing went wrong' })
@@ -71,7 +82,16 @@ const editUser = async (req, res) => {
 
 const deleteUser = async (req, res) => {
   try {
-    const deletedUser = await User.findByIdAndDelete({ _id: req.query._id });
+    const deletedUser = await User.findById({ _id: req.query._id });
+    if (deletedUser.role === 'Admin') {
+      if (getJwtInfo(req).role === 'Admin') {
+        deletedUser.remove();
+      } else {
+        res.status(400).json({ error: "Request cannot be performed" })
+      }
+    } else {
+      deletedUser.remove();
+    }
     res.status(200).json({ deletedUser })
   } catch (err) {
     res.status(500).json({ error: 'Somthing went wrong!' })
--- routes/roleRoutes/roleRoutes.js
+++ routes/roleRoutes/roleRoutes.js
@@ -10,7 +10,7 @@ router.get('/getarole/:rolename', roleControllers.getRole)// get a role ////////
 
 router.post('/add_role', checkPermission('P0101'), roleControllers.createRole)// add a role /////////////////////
 
-router.patch('/add_permission_to_role', checkPermission('P0102'), roleControllers.addPermissionToRole)// add a permission to a role //////////////////
+router.patch('/add_permission_to_role',  roleControllers.addPermissionToRole)// add a permission to a role //////////////////
 
 router.patch('/remove_permission_from_role', checkPermission('P0102'), roleControllers.removePermissionFromRole)// remove a permission from role///
 
